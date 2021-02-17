508a363ad9e3a9a616c7d3979fd9c9079fbbd09c
Gerard <gdavid32@hotmail.com>
2021-02-16 23:58:59 -0400

    Added the true and false button functionality and display the correct data base information in app textView, changed the layout.

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
@@ -15,23 +18,28 @@ public class MainActivity extends Activity {
     // TODO: Declare member variables here:
     Button mTrueButton;
     Button mFalseButton;
+    TextView mQuestion_TextView;
+    TrueFalse mCurrentQuestion;
+    int mQuestion;
+    int mIndex;
+    int mScore = 0;
 
     // TODO: Uncomment to create question bank
-//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-//            new TrueFalse(R.string.question_1, true),
-//            new TrueFalse(R.string.question_2, true),
-//            new TrueFalse(R.string.question_3, true),
-//            new TrueFalse(R.string.question_4, true),
-//            new TrueFalse(R.string.question_5, true),
-//            new TrueFalse(R.string.question_6, false),
-//            new TrueFalse(R.string.question_7, true),
-//            new TrueFalse(R.string.question_8, false),
-//            new TrueFalse(R.string.question_9, true),
-//            new TrueFalse(R.string.question_10, true),
-//            new TrueFalse(R.string.question_11, false),
-//            new TrueFalse(R.string.question_12, false),
-//            new TrueFalse(R.string.question_13,true)
-//    };
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13,true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,18 +48,18 @@ public class MainActivity extends Activity {
 
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
+        mQuestion_TextView = findViewById(R.id.question_text_view);
+
+        mCurrentQuestion = mQuestionBank[mIndex];
+        mQuestion = (mCurrentQuestion.getQuestionID());
+        mQuestion_TextView.setText(mQuestion);
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("Quizzler", "True button was clicked");
-//                Toast trueToast = Toast.makeText(getApplicationContext(),
-//                        "True button pressed!", Toast.LENGTH_SHORT);
-//                trueToast.show();
-
-                Toast.makeText(getApplicationContext(),
-                        "True button pressed!",
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
+                nextQuestion();
             }
         });
 
@@ -59,11 +67,37 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
                 Log.d("Quizzler", "False button was clicked");
-                Toast.makeText(getApplicationContext(),
-                        "False button pressed!",
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
+                nextQuestion();
             }
         });
 
+    }
+
+    private void nextQuestion() {
+        mIndex = (mIndex + 1) % mQuestionBank.length;
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestion_TextView.setText(mQuestion);
+    }
+
+    private void checkAnswer(boolean userSelection) {
+
+        mCurrentQuestion = mQuestionBank[mIndex];
+        boolean correctAnswer = mCurrentQuestion.isAnswer();
+
+        if (userSelection == correctAnswer) {
+
+            mScore++;
+
+            Toast.makeText(getApplicationContext(),
+                    R.string.correct_toast,
+                    Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            Toast.makeText(getApplicationContext(),
+                    R.string.incorrect_toast,
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
